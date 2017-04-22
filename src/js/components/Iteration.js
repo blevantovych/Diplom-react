195538bd84899c650f491be7d83786e16f7aee91
bodya17 <levantovychbogdan@gmail.com>
2017-04-22 14:57:55 +0300

    add plotly

--- src/js/components/Iteration.js
+++ src/js/components/Iteration.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Plot from './Plot';
 
 export default class Iteration extends React.Component {
     constructor(props) {
@@ -7,10 +8,15 @@ export default class Iteration extends React.Component {
 
     render() {
         return (
-            <div>
-                <h4 style={{color: 'green'}}>Iteration {this.props.ctn}</h4>
+            <div style={{border: '1px dashed gray', marginBottom: '10px'}}>
+                <h4 style={{color: 'green'}}>Ітерація {this.props.ctn+1}</h4>
                 <p>{this.props.data.alternance.map(el => el.toFixed(3)).join(' ')}</p>
-                <h4>Значення <i>x</i> в якому досягається максимальна похибка</h4>
+                <h4>Максимальна похибка
+                    <b>{this.props.data.max_err.toFixed(5)}</b>
+                </h4>
+                <h4>Значення <i>x</i> в якому досягається максимальна похибка &nbsp;
+                    <b>{this.props.data.x_of_max_err.toFixed(5)}</b></h4>
+                <Plot id={this.props.ctn+1}/>
             </div>
         );
     }
