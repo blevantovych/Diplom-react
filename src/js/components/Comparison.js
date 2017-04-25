03183df0450c6a0405456f204437ff5593643bdc
bodya17 <levantovychbogdan@gmail.com>
2017-04-25 21:39:45 +0300

    add graphs at comparison

--- src/js/components/Comparison.js
+++ src/js/components/Comparison.js
@@ -5,6 +5,7 @@ import Form from './Form';
 import { Table, TableBody, TableRow, TableRowColumn } from 'material-ui/Table';
 import { Card, CardText } from 'material-ui/Card';
 import Loader from './loader';
+import Plot from './Plot';
 
 if (!Array.prototype.last){
     Array.prototype.last = function(){
@@ -16,14 +17,22 @@ class Comparison extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            lssq: 0,
-            minmax: 0,
+            lssq: {
+                max_error: 0,
+                x_of_max_error: 0
+            },
+            minmax: {
+                max_err: 0,
+                x_of_max_err: 0,
+                func_plot: [],
+                pol_plot: []
+            },
             loaderActive: false
         }
-        this.getMaxErrs = this.getMaxErrs.bind(this);
     }
 
-    getMaxErrs(func, start, end, deg, precision) {
+    getMaxErrs = (func, start, end, deg, precision) => {
+
         this.setState({loaderActive: true});
         console.log('getting max errs');
         const lssq = () => fetch('https://least-squares.herokuapp.com/least_squares', {
@@ -35,38 +44,63 @@ class Comparison extends Component {
                                 .then(res => res.json())
 
         Promise.all([lssq(), minmax()]).then(data => {
-            console.log(data)
+            console.log(data);
             this.setState({
-                lssq: data[0].max_error.toFixed(4),
-                minmax: toArr(data[1]).last().max_err.toFixed(4), 
+                lssq: data[0],
+                minmax: toArr(data[1]).last(),
                 loaderActive: false
             })
         })
     }
 
     render() {
+
+        const lsPlot = <Plot id="comp_ls_plot" legend={false}
+                            plotData={[
+                                {x: this.state.lssq.x_approx, y: this.state.lssq.f_x_approx},
+                                {x: this.state.lssq.x_approx, y: this.state.lssq.approximation}
+                            ]}
+                        />
+        const mmPlot = <Plot id="comp_mm_plot" legend={false}
+                            plotData={[
+                                {x: this.state.minmax.func_plot[0], y: this.state.minmax.func_plot[1]},
+                                {x: this.state.minmax.pol_plot[0], y: this.state.minmax.pol_plot[1]}
+                            ]}
+                        />
         return (
             <div>
                 <Form onCalcClick={this.getMaxErrs}/>
                 <Loader active={this.state.loaderActive}/>
-                <Card>
-                    <CardText>
-                        <Table>
-                            <TableBody displayRowCheckbox={false}>
-                                <TableRow>
-                                    <TableRowColumn></TableRowColumn>
-                                    <TableRowColumn>Мінімакс</TableRowColumn>
-                                    <TableRowColumn>МНК</TableRowColumn>
-                                </TableRow>
-                                <TableRow>
-                                    <TableRowColumn>Макс похибка</TableRowColumn>
-                                    <TableRowColumn>{this.state.minmax}</TableRowColumn>
-                                    <TableRowColumn>{this.state.lssq}</TableRowColumn>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </CardText>
-                </Card>
+                <div style={{width: '90vw', position: "absolute", left: '-15vw'}}>
+                    <Card>
+                        <CardText>
+                            <Table>
+                                <TableBody displayRowCheckbox={false}>
+                                    <TableRow>
+                                        <TableRowColumn width={'20%'}></TableRowColumn>
+                                        <TableRowColumn>Мінімакс</TableRowColumn>
+                                        <TableRowColumn>МНК</TableRowColumn>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableRowColumn width={'20%'}>Макс похибка</TableRowColumn>
+                                        <TableRowColumn>{this.state.minmax.max_err.toFixed(4)}</TableRowColumn>
+                                        <TableRowColumn>{this.state.lssq.max_error.toFixed(4)}</TableRowColumn>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableRowColumn width={'20%'}>x в якому макс похибка</TableRowColumn>
+                                        <TableRowColumn>{this.state.minmax.x_of_max_err.toFixed(4)}</TableRowColumn>
+                                        <TableRowColumn>{this.state.lssq.x_of_max_error.toFixed(4)}</TableRowColumn>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableRowColumn width={'20%'}>Графіки</TableRowColumn>
+                                        <TableRowColumn>{mmPlot}</TableRowColumn>
+                                        <TableRowColumn>{lsPlot}</TableRowColumn>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </CardText>
+                    </Card>
+                </div>
             </div>
         );
     }
--- src/js/components/Plot.js
+++ src/js/components/Plot.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-// import Plotly from 'plotly';
 
 class Plot extends Component {
     constructor(props) {
         super(props);
     }
 
+    renderPlot = () => {
+        if (this.props.legend === false) {
+            Plotly.newPlot('plot' + this.props.id, this.props.plotData, {showlegend:false});
+        } else {
+            Plotly.newPlot('plot' + this.props.id, this.props.plotData, {title: this.props.title});
+        }
+    }
+
     componentDidUpdate(prevProps, prevState) {
-        Plotly.newPlot('plot' + this.props.id, this.props.plotData, {title: this.props.title});
+        this.renderPlot();
     }
 
     componentDidMount() {
-        Plotly.newPlot('plot' + this.props.id, this.props.plotData, {title: this.props.title});
+        this.renderPlot();
     }
 
     render() {
