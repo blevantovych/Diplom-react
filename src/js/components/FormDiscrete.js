c419929a5da6e8bca8f2d7f8a23e7d88876015da
bodya17 <levantovychbogdan@gmail.com>
2017-05-17 00:07:16 +0300

    add ability to upload data from csv file for lssq discrete

--- src/js/components/FormDiscrete.js
+++ src/js/components/FormDiscrete.js
@@ -6,6 +6,22 @@ class Form extends PureComponent {
 
     constructor(props) {
         super(props);
+        this.state = {
+            x_vals: '',
+            y_vals: ''
+        }
+    }
+
+    onFileUpload = (event) => {
+        var input = event.target
+        var reader = new FileReader()
+        reader.onload = () => {
+            var data = reader.result
+            let rows = data.split('\n')
+            this.setState({ x_vals: rows[0].split(/,\s?/g).join(' ')})
+            this.setState({ y_vals: rows[1].split(/,\s?/g).join(' ')})
+        }
+        reader.readAsText(input.files[0])
     }
 
     render() {
@@ -14,15 +30,17 @@ class Form extends PureComponent {
                 <TextField
                     floatingLabelText="Значення x"
                     type="text"
-                    defaultValue={this.props.formData.x_vals}
-                    onChange={(e) => this.props.formData.x_vals = e.target.value.split(' ').map(n => +n)}
+                    ref="x_input"
+                    value={this.state.x_vals}
+                    onChange={(e) => this.setState({x_vals: e.target.value})}
                 />
 
                 <TextField
                     floatingLabelText="Значення y"
                     type="text"
-                    defaultValue={this.props.formData.y_vals}
-                    onChange={(e) => this.props.formData.y_vals = e.target.value.split(' ').map(n => +n)}
+                    ref="y_input"
+                    value={this.state.y_vals}
+                    onChange={(e) => this.setState({y_vals: e.target.value})}
                 />
 
                 <TextField
@@ -32,10 +50,23 @@ class Form extends PureComponent {
                     onChange={(e) => this.props.formData.deg = e.target.value}
                 />
 
+                <RaisedButton label="Завантажити CSV"
+                    secondary={true}
+                    containerElement="label"
+                ><input
+                    style={{"cursor":"pointer","position":"absolute","top":"0","bottom":"0","right":"0","left":"0","width":"100%","opacity":"0"}}
+                    type="file"
+                    onChange={this.onFileUpload} />
+                </RaisedButton>
+
                  <RaisedButton label="Обчислити"
                     primary={true}
                     disabled={this.props.formData.disabled}
-                    onClick={() => this.props.onCalcClick(this.props.formData.x_vals, this.props.formData.y_vals, +this.props.formData.deg)}
+                    onClick={() => {
+                        this.props.formData.x_vals = this.state.x_vals.split(' ').map(n => +n)
+                        this.props.formData.y_vals = this.state.y_vals.split(' ').map(n => +n)
+                        this.props.onCalcClick(this.props.formData.x_vals, this.props.formData.y_vals, +this.props.formData.deg)
+                    }}
                 />
 
             </div>
